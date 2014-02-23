321041a4d4f2909bcfd072754eb05d00c8af4918
Simon Strom <simon.strom@gmail.com>
2014-02-24 00:01:39 +0100

    style new bet

--- client/js/profile.js
+++ client/js/profile.js
@@ -1,3 +1,7 @@
 Template.profile.betcoins = function() {
 	return Meteor.user().profile.betcoins;
 }
+
+Template.betsForUser.bets = function() {
+	return Bets.find({created_by:Meteor.user()._id});
+}
