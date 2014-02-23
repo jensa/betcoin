2b5f0d420d6c321e01f698a4aeeaa1a315bb1fbf
Jens Arvidsson <jensarv@gmail.com>
2014-02-23 15:51:05 +0100

    merge

--- client/client.js
+++ client/client.js
@@ -1,11 +1,15 @@
 Template.start.bets = function (){
-	console.log("hejh");
-	return Bets.find();
+	return Bets.find({"users.userId":Meteor.user().profile.id});
 }
+
 Template.start.events({
   'click input' : function () {
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
       console.log("You pressed the button");
+  	console.log(Meteor.user().profile.id);
+  },
+  'click #addBet' : function () {
+  	Bets.insert({text:text, options:options, users:[Meteor.user().profile.id]});
   }
 });
